6c73d9fadc5bebf4deb5fbc064fdbad17756595b
wsdf25867 <wsdf25867@gmail.com>
2025-12-05 20:45:10 +0900

    Initialize Spring Boot project with Kotlin support, Gradle build system, and default configurations.

---
+++ src/main/kotlin/com/wherewhear/api/WherewearApplication.kt
@@ -0,0 +1,11 @@
+package com.wherewhear.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WherewearApplication
+
+fun main(args: Array<String>) {
+    runApplication<WherewearApplication>(*args)
+}
---
+++ src/test/kotlin/com/wherewhear/api/WherewearApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wherewhear.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WherewearApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
